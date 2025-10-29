bde4aabcf9d10d4ea5aecc934fefcad165ce5391
Javier Pozo Miranda <javier.pozo.miranda@gmail.com>
2025-10-28 22:38:37 -0500

    heavy changes. important base.html updated

--- Hestia_Production/hestia_app/blueprints/dashboard/routes.py
+++ Hestia_Production/hestia_app/blueprints/dashboard/routes.py
@@ -1,6 +1,12 @@
-# hestia_app/blueprints/dashboard/routes.py
 from flask import Blueprint, render_template
-bp = Blueprint("dashboard", __name__, url_prefix="/home", template_folder="templates")  # ✅
-@bp.get("/")
+
+bp = Blueprint(
+    "dashboard",
+    __name__,
+    url_prefix="/home",
+    template_folder="templates"
+)
+
+@bp.get("/", endpoint="dashboard")  # ← alias: ahora existe endpoint 'dashboard'
 def dashboard_home():
     return render_template("dashboards/dashboard_gerente.html")
